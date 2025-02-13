f222a6b947a5aac562c0adebaa31f7b016cc2194
FreeVersion <89584262+Kenzienub@users.noreply.github.com>
2025-02-13 12:39:52 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -3,94 +3,94 @@ if id == 2753915549 then First_Sea = true; elseif id == 4442272183 then Second_S
 
 function MaterialMon()
     if SelectMaterial == "Radioactive Material" then
-        MMon = "Factory Staff"
-        MPos = CFrame.new(295,73,-56)
-        SP = "Default"
+        _G.MMon = "Factory Staff"
+        _G.MPos = CFrame.new(295,73,-56)
+        _G.SP = "Default"
     elseif SelectMaterial == "Mystic Droplet" then
-        MMon = "Water Fighter"
-        MPos = CFrame.new(-3385,239,-10542)
-        SP = "Default"
+        _G.MMon = "Water Fighter"
+        _G.MPos = CFrame.new(-3385,239,-10542)
+        _G.SP = "Default"
     elseif SelectMaterial == "Magma Ore" then
         if First_Sea then
-            MMon = "Military Spy"
-            MPos = CFrame.new(-5815,84,8820)
-            SP = "Default"
+           _G.MMon = "Military Spy"
+            _G.MPos = CFrame.new(-5815,84,8820)
+            _G.SP = "Default"
         elseif Second_Sea then
-            MMon = "Magma Ninja"
-            MPos = CFrame.new(-5428,78,-5959)
-            SP = "Default"
+            _G.MMon = "Magma Ninja"
+            _G.MPos = CFrame.new(-5428,78,-5959)
+            _G.SP = "Default"
         end
     elseif SelectMaterial == "Angel Wings" then
-        MMon = "God's Guard"
-        MPos = CFrame.new(-4698,845,-1912)
-        SP = "Default"
+        _G.MMon = "God's Guard"
+        _G.MPos = CFrame.new(-4698,845,-1912)
+        _G.SP = "Default"
         if (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - Vector3.new(-7859.09814, 5544.19043, -381.476196)).Magnitude >= 5000 then
             game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("requestEntrance",Vector3.new(-7859.09814, 5544.19043, -381.476196))
         end
     elseif SelectMaterial == "Leather" then
         if First_Sea then
-            MMon = "Brute"
-            MPos = CFrame.new(-1145,15,4350)
-            SP = "Default"
+            _G.MMon = "Brute"
+            _G.MPos = CFrame.new(-1145,15,4350)
+            _G.SP = "Default"
         elseif Second_Sea then
-            MMon = "Marine Captain"
-            MPos = CFrame.new(-2010.5059814453125, 73.00115966796875, -3326.620849609375)
-            SP = "Default"
+            _G.MMon = "Marine Captain"
+            _G.MPos = CFrame.new(-2010.5059814453125, 73.00115966796875, -3326.620849609375)
+            _G.SP = "Default"
         elseif Third_Sea then
-            MMon = "Jungle Pirate"
-            MPos = CFrame.new(-11975.78515625, 331.7734069824219, -10620.0302734375)
-            SP = "Default"
+            _G.MMon = "Jungle Pirate"
+            _G.MPos = CFrame.new(-11975.78515625, 331.7734069824219, -10620.0302734375)
+            _G.SP = "Default"
         end
     elseif SelectMaterial == "Scrap Metal" then
         if First_Sea then
-            MMon = "Brute"
-            MPos = CFrame.new(-1145,15,4350)
-            SP = "Default"
+            _G.MMon = "Brute"
+            _G.MPos = CFrame.new(-1145,15,4350)
+            _G.SP = "Default"
         elseif Second_Sea then
-            MMon = "Swan Pirate"
-            MPos = CFrame.new(878,122,1235)
-            SP = "Default"
+            _G.MMon = "Swan Pirate"
+            _G.MPos = CFrame.new(878,122,1235)
+            _G.SP = "Default"
         elseif Third_Sea then
-            MMon = "Jungle Pirate"
-            MPos = CFrame.new(-12107,332,-10549)
-            SP = "Default"
+            _G.MMon = "Jungle Pirate"
+            _G.MPos = CFrame.new(-12107,332,-10549)
+            _G.SP = "Default"
         end
     elseif SelectMaterial == "Fish Tail" then
         if Third_Sea then
-            MMon = "Fishman Raider"
-            MPos = CFrame.new(-10993,332,-8940)
-            SP = "Default"
+            _G.MMon = "Fishman Raider"
+            _G.MPos = CFrame.new(-10993,332,-8940)
+            _G.SP = "Default"
         elseif First_Sea then
-            MMon = "Fishman Warrior"
-            MPos = CFrame.new(61123,19,1569)
-            SP = "Default"
+            _G.MMon = "Fishman Warrior"
+            _G.MPos = CFrame.new(61123,19,1569)
+            _G.SP = "Default"
             if (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - Vector3.new(61163.8515625, 5.342342376708984, 1819.7841796875)).Magnitude >= 17000 then
                 game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("requestEntrance",Vector3.new(61163.8515625, 5.342342376708984, 1819.7841796875))
             end
         end
     elseif SelectMaterial == "Demonic Wisp" then
-        MMon = "Demonic Soul"
-        MPos = CFrame.new(-9507,172,6158)
-        SP = "Default"
+       _G.MMon = "Demonic Soul"
+        _G.MPos = CFrame.new(-9507,172,6158)
+        _G.SP = "Default"
     elseif SelectMaterial == "Vampire Fang" then
-        MMon = "Vampire"
-        MPos = CFrame.new(-6033,7,-1317)
-        SP = "Default"
+        _G.MMon = "Vampire"
+        _G.MPos = CFrame.new(-6033,7,-1317)
+        _G.SP = "Default"
     elseif SelectMaterial == "Conjured Cocoa" then
-        MMon = "Chocolate Bar Battler"
-        MPos = CFrame.new(620.6344604492188,78.93644714355469, -12581.369140625)
-        SP = "Default"
+        _G.MMon = "Chocolate Bar Battler"
+        _G.MPos = CFrame.new(620.6344604492188,78.93644714355469, -12581.369140625)
+        _G.SP = "Default"
     elseif SelectMaterial == "Dragon Scale" then
-        MMon = "Dragon Crew Archer"
-        MPos = CFrame.new(6594,383,139)
-        SP = "Default"
+        _G.MMon = "Dragon Crew Archer"
+        _G.MPos = CFrame.new(6594,383,139)
+        _G.SP = "Default"
     elseif SelectMaterial == "Gunpowder" then
-        MMon = "Pistol Billionaire"
-        MPos = CFrame.new(-469,74,5904)
-        SP = "Default"
+        _G.MMon = "Pistol Billionaire"
+        _G.MPos = CFrame.new(-469,74,5904)
+        _G.SP = "Default"
     elseif SelectMaterial == "Mini Tusk" then
-        MMon = "Mythological Pirate"
-        MPos = CFrame.new(-13545,470,-6917)
-        SP = "Default"
+        _G.MMon = "Mythological Pirate"
+        _G.MPos = CFrame.new(-13545,470,-6917)
+        _G.SP = "Default"
     end
 end
